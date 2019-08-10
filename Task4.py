dc4c76ffd556e3ae5f64154b37f16520d0f0c81d
Aleksandr <Aleksandrsanya@gmail.com>
2019-08-10 11:55:17 +0300

    Lesson1

---
+++ Task4.py
@@ -0,0 +1,5 @@
+n = int(input("Введіть число n:"))
+m = 2
+while m < n:
+    print(m, end=" ")
+    m = m + 2
---
+++ Task5.py
@@ -0,0 +1,5 @@
+R1 = float(input("Enter R1 :"))
+T1 = float(input("Enter T1 :"))
+R2 = float(input("Enter R2 :"))
+T2 =(T1 **2 * R2 **3 / R1 **3)**0.5
+print("T2 =",T2)
